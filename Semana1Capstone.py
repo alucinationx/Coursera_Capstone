e4934fa81d9adfbc3ac329194db1ec90783a7470
Daniel Vasconcelos <danielvasconcelos7@hotmail.com>
2021-04-12 11:10:59 -0300

    Add files via upload

---
+++ Semana1Capstone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Project Cousera 'Capstone'</h1>
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
